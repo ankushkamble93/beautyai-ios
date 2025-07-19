db33191092c519f1801d74afd3a5098cfb0c9e1f
Ankush Kamble <ankush_kamble@Ankushs-MacBook.local>
2025-07-19 18:23:54 -0400

    Fix BeautyAIApp.swift - add complete app entry point with Firebase configuration

--- BeautyAI/BeautyAIApp.swift
+++ BeautyAI/BeautyAIApp.swift
@@ -1,1 +1,22 @@
 import SwiftUI
+import Firebase
+
+@main
+struct BeautyAIApp: App {
+    @StateObject private var authManager = AuthenticationManager()
+    @StateObject private var skinAnalysisManager = SkinAnalysisManager()
+    @StateObject private var chatManager = ChatManager()
+    
+    init() {
+        FirebaseApp.configure()
+    }
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environmentObject(authManager)
+                .environmentObject(skinAnalysisManager)
+                .environmentObject(chatManager)
+        }
+    }
+}
